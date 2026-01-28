e0d3ff2517868268ae3ffbaadeaeeec47253654b
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-01-28 15:41:51 -0700

    Create functions to play/pause video and skip ahead

--- js/script.js
+++ js/script.js
@@ -7,3 +7,27 @@ const toggle = player.querySelector('.toggle');
 const skipBtns = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
+function togglePlay() {
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+    // if (video.paused) {
+    //     video.play();
+    // } else {
+    //     video.pause();
+    // }
+}
+
+function updateBtn() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
+function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateBtn);
+video.addEventListener('pause', updateBtn);
+toggle.addEventListener('click', togglePlay);
+skipBtns.forEach(btn => btn.addEventListener('click', skip));
