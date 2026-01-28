6352da97d484bd6b306ba5bd8b8fad67b34cacd0
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-01-28 16:02:03 -0700

    Create function for scrubbing the progress bar

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progressBar = player.querySelector('.progress');
+const progressFilled = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipBtns = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
@@ -26,8 +27,26 @@ function skip() {
     video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressFilled.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progressBar.offsetWidth) * video.duration;
+    video.currentTime = scrubTime
+}
+
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateBtn);
 video.addEventListener('pause', updateBtn);
+video.addEventListener('timeupdate', handleProgress);
 toggle.addEventListener('click', togglePlay);
 skipBtns.forEach(btn => btn.addEventListener('click', skip));
+ranges.forEach(slider => slider.addEventListener('change', handleRangeUpdate));
+ranges.forEach(slider => slider.addEventListener('mousemove', handleRangeUpdate));
+progressBar.addEventListener('click', scrub);
