d36c18625661942b4c19cbce41ca13cb4b6f1e37
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-01-28 14:13:15 -0700

    Add new video and select video elements

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Get video elements
+
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progressBar = player.querySelector('.progress');
+const toggle = player.querySelector('.toggle');
+const skipBtns = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
